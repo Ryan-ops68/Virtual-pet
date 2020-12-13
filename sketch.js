538ce1c81344a091d299bcc6e20cbe5776a70882
Ryan-ops68 <72042413+Ryan-ops68@users.noreply.github.com>
2020-12-13 14:34:42 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCt8jM1JEfPW4K_WsF1w1FCJGz4rR4hWhE",
+    authDomain: "virtual-pet-bac92.firebaseapp.com",
+    databaseURL: "https://virtual-pet-bac92-default-rtdb.firebaseio.com",
+    projectId: "virtual-pet-bac92",
+    storageBucket: "virtual-pet-bac92.appspot.com",
+    messagingSenderId: "783334554186",
+    appId: "1:783334554186:web:00e39b879b1763eaf544b3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
